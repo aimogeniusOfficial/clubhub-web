6ad00ce8a892fbdf62da2ade9f11d28c95087cb4
Eldar Hasanov <eldarh079@berkeley.edu>
2023-10-28 06:51:00 -0700

    reverted the public routes

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -4,9 +4,9 @@ import Auth from 'pages/public/auth/Auth';
 import RequestReset from 'pages/public/auth/RequestReset';
 import ResetPassword from 'pages/public/auth/ResetPassword';
 import SuccessPage from 'pages/public/auth/SuccessPage';
-import Landing from 'pages/public/landing/Landing';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
+import PublicLayout from './layouts/PublicLayout';
 import { RoutesMap } from './types';
 
 export const authRoutesMap: RoutesMap = {
@@ -18,16 +18,22 @@ export const authRoutesMap: RoutesMap = {
 };
 
 const PublicRoutes = (): JSX.Element => {
-  // In case we need it
-
-  // const checkIsAuthorizationLayout = (path: string): boolean => {
-  //   return ['/login', '/register', '/request-password', '/reset-password'].includes(path);
-  // };
+  const checkIsAuthorizationLayout = (path: string): boolean => {
+    return ['/login', '/register', '/request-password', '/reset-password'].includes(path);
+  };
 
   return (
     <Routes>
       {Object.keys(authRoutesMap).map(path => (
-        <Route key={path} path={path} element={<Landing />} />
+        <Route
+          key={path}
+          path={path}
+          element={
+            <PublicLayout isAuthorizationLayout={checkIsAuthorizationLayout(path)}>
+              {authRoutesMap[path]}
+            </PublicLayout>
+          }
+        />
       ))}
     </Routes>
   );
